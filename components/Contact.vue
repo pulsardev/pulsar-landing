e9664debcdfd7ec8ccf185639e0873a3d4bb86ce
Mathieu Morainville <mathieumorainville@hotmail.com>
2017-08-27 21:04:06 +0200

    feat: validate the form and send the message with Formspree

--- components/Contact.vue
+++ components/Contact.vue
@@ -3,38 +3,42 @@
     <div class="container">
       <h1 class="title is-spaced has-text-centered has-text-grey-lighter">Contact Us</h1>
 
-      <div class="field">
-        <label class="label">Name</label>
-        <div class="control">
-          <input class="input" type="text" placeholder="Name">
+      <form action="https://formspree.io/pulsar.dev.team@gmail.com" method="POST">
+        <div class="field">
+          <label class="label" for="name">Name</label>
+          <div class="control">
+            <input v-validate="'required'" v-model="name" class="input" :class="{ 'is-danger': errors.has('name') }" id="name" name="name" type="text" placeholder="Name" required>
+          </div>
+          <span v-show="errors.has('name')" class="help is-danger">{{ errors.first('name') }}</span>
         </div>
-      </div>
 
-      <div class="field">
-        <label class="label" for="email">Email</label>
-        <div class="control has-icons-left has-icons-right">
-          <input v-validate="'required|email'" class="input" :class="{ 'is-danger': errors.has('email') }" name="email" type="text" placeholder="Email">
-          <span class="icon is-small is-left has-text-grey"><i class="fa fa-envelope"></i></span>
-          <span v-show="errors.has('email')" class="icon is-small is-right has-text-grey"><i class="fa fa-warning"></i></span>
+        <div class="field">
+          <label class="label" for="email">Email</label>
+          <div class="control has-icons-left has-icons-right">
+            <input v-validate="'required|email'" v-model="email" class="input" :class="{ 'is-danger': errors.has('_replyto') }" id="email" name="_replyto" type="text" placeholder="Email" required>
+            <span class="icon is-small is-left has-text-grey"><i class="fa fa-envelope"></i></span>
+            <span v-show="errors.has('_replyto')" class="icon is-small is-right has-text-grey"><i class="fa fa-warning"></i></span>
+          </div>
+          <span v-show="errors.has('_replyto')" class="help is-danger">{{ errors.first('_replyto') }}</span>
         </div>
-        <span v-show="errors.has('email')" class="help is-danger">{{ errors.first('email') }}</span>
-      </div>
 
-      <div class="field">
-        <label class="label">Message</label>
-        <div class="control">
-          <textarea class="textarea" placeholder="Message"></textarea>
+        <div class="field">
+          <label class="label">Message</label>
+          <div class="control">
+            <textarea v-validate="'required'" v-model="message" class="textarea" :class="{ 'is-danger': errors.has('message') }" id="message" name="message" placeholder="Message" required></textarea>
+          </div>
+          <span v-show="errors.has('message')" class="help is-danger">{{ errors.first('message') }}</span>
         </div>
-      </div>
 
-      <div class="field is-grouped">
-        <div class="control">
-          <button class="button is-primary">Submit</button>
+        <div class="field is-grouped">
+          <div class="control">
+            <input type="submit" class="button is-primary" value="Submit">
+          </div>
+          <div class="control">
+            <button class="button is-light" @click="cancel">Cancel</button>
+          </div>
         </div>
-        <div class="control">
-          <button class="button is-light">Cancel</button>
-        </div>
-      </div>
+      </form>
     </div>
   </section>
 </template>
@@ -42,7 +46,21 @@
 <script>
   export default {
     $validates: true,
-    name: 'contact'
+    name: 'contact',
+    data () {
+      return {
+        name: '',
+        email: '',
+        message: ''
+      }
+    },
+    methods: {
+      cancel () {
+        this.name = ''
+        this.email = ''
+        this.message = ''
+      }
+    }
   }
 </script>
 
